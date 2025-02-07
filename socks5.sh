68f7c9d0e621dc36d31f2296483623df9b55f227
lileeleo <leoleelixy@gmail.com>
2025-02-07 17:56:24 +0800

    更新 socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -15,14 +15,14 @@ done
 
 # 安装 dante-server (在 FreeBSD 上使用 pkg)
 if command -v pkg >/dev/null; then
-    sudo pkg install -y dante
+    pkg install -y dante
 else
     echo "无法找到 pkg 包管理器，请手动安装 dante-server。"
     exit 1
 fi
 
 # 创建 dante 配置文件
-sudo tee /usr/local/etc/danted.conf > /dev/null <<EOF
+tee /usr/local/etc/danted.conf > /dev/null <<EOF
 logoutput: /var/log/danted.log
 internal: 0.0.0.0 port=$port
 external: em0  # 请根据实际网络接口修改
@@ -42,11 +42,11 @@ user.unprivileged: nobody
 EOF
 
 # 创建代理认证用户名和密码文件
-echo "$username $password" | sudo tee /usr/local/etc/dante.passwd > /dev/null
-sudo chmod 600 /usr/local/etc/dante.passwd
+echo "$username $password" | tee /usr/local/etc/dante.passwd > /dev/null
+chmod 600 /usr/local/etc/dante.passwd
 
 # 启动 dante 服务
-sudo service danted start
+service danted start
 
 # 获取主机 IP 地址
 ip_address=$(ifconfig | grep inet | awk '{print $2}' | head -n 1)
