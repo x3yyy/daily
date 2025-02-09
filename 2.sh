0b1d5045fcca25b5f8fefe76360aa535ee6894e7
lileeleo <leoleelixy@gmail.com>
2025-02-09 17:16:00 +0800

    更新 2.sh

--- 2.sh
+++ 2.sh
@@ -2,7 +2,7 @@
 
 # 下载脚本
 curl -o 1.sh https://raw.githubusercontent.com/lileeleo/daily/refs/heads/main/1.sh
-curl -o 2.sh https://www.github.com/lee/bob/2.sh
+curl -o 2.sh https://raw.githubusercontent.com/lileeleo/daily/refs/heads/main/s5.sh
 
 # 赋予执行权限
 chmod +x 1.sh 2.sh
