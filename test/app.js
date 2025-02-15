151a2714f0920e0809459d744858fcfb80ad29c6
lileeleo <leoleelixy@gmail.com>
2025-02-15 09:58:46 +0800

    更新 app.js

--- test/app.js
+++ test/app.js
@@ -47,7 +47,7 @@ const services = [
   },
   {
     name: 'S5',
-    pattern: 's5 -c /home/chqlileoleeyu/.s5/config.json',
+    pattern: '.s5/config.json',
     startCmd: 'nohup ~/.s5/s5 -c ~/.s5/config.json >/dev/null 2>&1 &',
   }
 ];
