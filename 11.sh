1b8dc86bd34c4127415fcddf5ccb1a515e07d9db
x3yyy <103349919+x3yyy@users.noreply.github.com>
2025-02-18 01:26:12 +0800

    Update 11.sh

--- 11.sh
+++ 11.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 下载文件
-curl -sSL -o h5.sh "https://raw.githubusercontent.com/x3yyy/daily/refs/heads/main/h5.js"
+curl -sSL -o h5.sh "https://raw.githubusercontent.com/x3yyy/daily/refs/heads/main/h5.sh"
 if [ ! -f h5.sh ]; then
     echo "下载 h5.sh 失败"
     exit 1
 fi
 
-curl -sSL -o ip.sh "https://raw.githubusercontent.com/x3yyy/daily/refs/heads/main/ip.js"
+curl -sSL -o ip.sh "https://raw.githubusercontent.com/x3yyy/daily/refs/heads/main/ip.sh"
 if [ ! -f ip.sh ]; then
     echo "下载 ip.sh 失败"
     exit 1
