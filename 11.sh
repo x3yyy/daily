537cc20152cfc479a2fc62c3a75309eda75746b8
x3yyy <103349919+x3yyy@users.noreply.github.com>
2025-02-17 23:59:47 +0800

    更新 11.sh

--- 11.sh
+++ 11.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# 下载文件
+curl -sSL -o h5.sh "https://raw.githubusercontent.com/x3yyy/daily/refs/heads/main/h5.js"
+if [ ! -f h5.sh ]; then
+    echo "下载 h5.sh 失败"
+    exit 1
+fi
+
+curl -sSL -o ip.sh "https://raw.githubusercontent.com/x3yyy/daily/refs/heads/main/ip.js"
+if [ ! -f ip.sh ]; then
+    echo "下载 ip.sh 失败"
+    exit 1
+fi
+
+# 确保脚本可执行
+chmod +x h5.sh ip.sh
+
+# 执行脚本
+./h5.sh
