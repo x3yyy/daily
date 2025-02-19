f3d29d052ce9b3b6a71f2279e93315e8ef7bda79
x3yyy <103349919+x3yyy@users.noreply.github.com>
2025-02-19 14:36:01 +0800

    更新 h5.sh

--- h5.sh
+++ h5.sh
@@ -356,7 +356,7 @@ fi
           fi
       fi
 
-      echo -e "\n\e[31msocks://${SOCKS5_USER}:${SOCKS5_PASS}@${SERV_DOMAIN}:${SOCKS5_PORT}\e[0m\n"
+      echo -e "\n\e[31m代理创建成功\e[0m\n"
     else
       echo "代理创建失败，请检查自己输入的内容。"
     fi
