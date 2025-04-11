07ea951dfde98a4279d67d66c358e5575d7b3951
x3yyy <103349919+x3yyy@users.noreply.github.com>
2025-04-12 02:25:43 +0800

    更新 1.sh

--- 1.sh
+++ 1.sh
@@ -2,133 +2,131 @@
 export LC_ALL=C
 HOSTNAME=$(hostname)
 USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
-MD5_HASH=$(echo -n "$USERNAME" | md5sum | awk '{print $1}')
-export UUID=${UUID:-${MD5_HASH:0:8}-${MD5_HASH:8:4}-4${MD5_HASH:12:3}-$(echo $((RANDOM % 4 + 8)) | head -c 1)${MD5_HASH:15:3}-${MD5_HASH:19:12}}
-export NEZHA_PORT=${NEZHA_PORT:-'5555'}             
-export NEZHA_KEY=${NEZHA_KEY:-''}                
-export PORT=${PORT:-''} 
+export UUID=${UUID:-$(echo -n "$USERNAME+$HOSTNAME" | md5sum | head -c 32 | sed -E 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')}
+export NEZHA_SERVER=${NEZHA_SERVER:-''}             
+export NEZHA_PORT=${NEZHA_PORT:-''}            
+export NEZHA_KEY=${NEZHA_KEY:-''}
 export CHAT_ID=${CHAT_ID:-''} 
 export BOT_TOKEN=${BOT_TOKEN:-''} 
-export SUB_TOKEN=${SUB_TOKEN:-'sub'}
+export UPLOAD_URL=${UPLOAD_URL:-''}
+export SUB_TOKEN=${SUB_TOKEN:-${UUID:0:8}}
 
-[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="$HOME/domains/${USERNAME}.ct8.pl/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.ct8.pl/public_html" || WORKDIR="$HOME/domains/${USERNAME}.serv00.net/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.serv00.net/public_html"
-rm -rf "$WORKDIR" && mkdir -p "$WORKDIR" "$FILE_PATH" && chmod 777 "$WORKDIR" "$FILE_PATH" >/dev/null 2>&1
+if [[ "$HOSTNAME" =~ ct8 ]]; then
+    CURRENT_DOMAIN="ct8.pl"
+elif [[ "$HOSTNAME" =~ useruno ]]; then
+    CURRENT_DOMAIN="useruno.com"
+else
+    CURRENT_DOMAIN="serv00.net"
+fi
+WORKDIR="${HOME}/domains/${USERNAME}.${CURRENT_DOMAIN}/logs"
+FILE_PATH="${HOME}/domains/${USERNAME}.${CURRENT_DOMAIN}/public_html"
+rm -rf "$WORKDIR" "$FILE_PATH" && mkdir -p "$WORKDIR" "$FILE_PATH" && chmod 777 "$WORKDIR" "$FILE_PATH" >/dev/null 2>&1
 bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
+command -v curl &>/dev/null && COMMAND="curl -so" || command -v wget &>/dev/null && COMMAND="wget -qO" || { red "Error: neither curl nor wget found, please install one of them." >&2; exit 1; }
 
-get_available_ports() {
-    # 获取所有端口列表
-    port_list=$(devil port list)
+check_port () {
+  clear
+  echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
+  port_list=$(devil port list)
+  tcp_ports=$(echo "$port_list" | grep -c "tcp")
+  udp_ports=$(echo "$port_list" | grep -c "udp")
 
-    # 获取当前可用的TCP和UDP端口数
-    tcp_ports=$(echo "$port_list" | grep -c "tcp")
-    udp_ports=$(echo "$port_list" | grep -c "udp")
+  if [[ $udp_ports -lt 1 ]]; then
+      echo -e "\e[1;91m没有可用的UDP端口,正在调整...\e[0m"
 
-    # 如果有可用的TCP和UDP端口
-    if [[ $tcp_ports -ge 1 && $udp_ports -ge 1 ]]; then
-        # 获取第一个可用的TCP和UDP端口
-        tcp_port=$(echo "$port_list" | awk '/tcp/ {print $1}' | sed -n '1p')
-        udp_port=$(echo "$port_list" | awk '/udp/ {print $1}' | sed -n '1p')
-        echo -e "\e[1;35m当前TCP端口: $tcp_port\e[0m"
-        echo -e "\e[1;35m当前UDP端口: $udp_port\e[0m"
+      if [[ $tcp_ports -ge 3 ]]; then
+          tcp_port_to_delete=$(echo "$port_list" | awk '/tcp/ {print $1}' | head -n 1)
+          devil port del tcp $tcp_port_to_delete
+          echo -e "\e[1;32m已删除TCP端口: $tcp_port_to_delete\e[0m"
+      fi
 
-    else
-        # 如果没有可用的UDP端口，尝试申请一个新的UDP端口
-        if [[ $udp_ports -lt 1 ]]; then
-            echo -e "\e[1;91m没有可用的UDP端口，正在申请...\e[0m"
-            while true; do
-                udp_port=$(shuf -i 10000-65535 -n 1)  # 随机选择一个UDP端口
-                result=$(devil port add udp $udp_port 2>&1)
-                if [[ $result == *"succesfully"* ]]; then
-                    echo -e "\e[1;32m已成功添加UDP端口: $udp_port"
-                    break
-                else
-                    echo -e "\e[1;33m端口 $udp_port 不可用，尝试其他端口...\e[0m"
-                fi
-            done
-        fi
+      while true; do
+          udp_port=$(shuf -i 10000-65535 -n 1)
+          result=$(devil port add udp $udp_port 2>&1)
+          if [[ $result == *"Ok"* ]]; then
+              echo -e "\e[1;32m已添加UDP端口: $udp_port"
+              udp_port1=$udp_port
+              break
+          else
+              echo -e "\e[1;33m端口 $udp_port 不可用，尝试其他端口...\e[0m"
+          fi
+      done
 
-        # 如果没有可用的TCP端口，尝试申请一个新的TCP端口
-        if [[ $tcp_ports -lt 1 ]]; then
-            echo -e "\e[1;91m没有可用的TCP端口，正在申请...\e[0m"
-            while true; do
-                tcp_port=$(shuf -i 10000-65535 -n 1)  # 随机选择一个TCP端口
-                result=$(devil port add tcp $tcp_port 2>&1)
-                if [[ $result == *"succesfully"* ]]; then
-                    echo -e "\e[1;32m已成功添加TCP端口: $tcp_port"
-                    break
-                else
-                    echo -e "\e[1;33m端口 $tcp_port 不可用，尝试其他端口...\e[0m"
-                fi
-            done
-        fi
-    fi
+      echo -e "\e[1;32m端口已调整完成,如安装完后节点不通,访问 /restart域名重启\e[0m"
+      devil binexec on >/dev/null 2>&1
+      kill -9 $(ps -o ppid= -p $$) >/dev/null 2>&1
+  else
+      udp_ports=$(echo "$port_list" | awk '/udp/ {print $1}')
+      udp_port1=$(echo "$udp_ports" | sed -n '1p')
 
-    # 输出最终的 TCP 和 UDP 端口
-    echo -e "\e[1;35m最终 TCP 端口: $tcp_port\e[0m"
-    echo -e "\e[1;35m最终 UDP 端口: $udp_port\e[0m"
+  fi
 
-    # 设置环境变量
-    export PORT=$udp_port
-    export SOCKS5_PORT=$tcp_port
+  export PORT=$udp_port1
+  echo -e "\e[1;35mhysteria2使用udp端口: $udp_port1\e[0m"
 }
+check_port
 
-get_available_ports
-
-echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
 ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
 if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
-    FILE_INFO=("https://download.hysteria.network/app/latest/hysteria-freebsd-arm64 web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
+    BASE_URL="https://github.com/eooce/test/releases/download/freebsd-arm64"
 elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
-    FILE_INFO=("https://download.hysteria.network/app/latest/hysteria-freebsd-amd64 web" "https://github.com/eooce/test/releases/download/freebsd/npm npm")
+    BASE_URL="https://github.com/eooce/test/releases/download/freebsd"
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
+FILE_INFO=("$BASE_URL/hy2 web")
+if [ -n "$NEZHA_PORT" ]; then
+    FILE_INFO+=("$BASE_URL/npm npm")
+else
+    FILE_INFO+=("$BASE_URL/v1 php")
+    NEZHA_TLS=$(case "${NEZHA_SERVER##*:}" in 443|8443|2096|2087|2083|2053) echo -n tls;; *) echo -n false;; esac)
+    cat > "${WORKDIR}/config.yaml" << EOF
+client_secret: ${NEZHA_KEY}
+debug: false
+disable_auto_update: true
+disable_command_execute: false
+disable_force_update: true
+disable_nat: false
+disable_send_query: false
+gpu: false
+insecure_tls: false
+ip_report_period: 1800
+report_delay: 1
+server: ${NEZHA_SERVER}
+skip_connection_count: false
+skip_procs_count: false
+temperature: false
+tls: ${NEZHA_TLS}
+use_gitee_to_upgrade: false
+use_ipv6_country_code: false
+uuid: ${UUID}
+EOF
+fi
 declare -A FILE_MAP
-
-# 固定文件名逻辑
-generate_fixed_name() {
-    local type=$1  # 传入文件类型，例如 "web" 或 "npm"
-    if [[ "$type" == "web" ]]; then
-        echo "hy2"  # 将 web 类型的文件固定命名为 hy2
-    elif [[ "$type" == "npm" ]]; then
-        echo "nezha"  # 将 npm 类型的文件固定命名为 nezha
-    else
-        echo "unknown"  # 其他情况命名为 unknown
-    fi
-}
-
-download_file() {
-    local URL=$1
-    local NEW_FILENAME=$2
-
-    if command -v curl >/dev/null 2>&1; then
-        curl -L -sS -o "$NEW_FILENAME" "$URL"
-        echo -e "\e[1;32mDownloaded $NEW_FILENAME by curl\e[0m"
-    elif command -v wget >/dev/null 2>&1; then
-        wget -q -O "$NEW_FILENAME" "$URL"
-        echo -e "\e[1;32mDownloaded $NEW_FILENAME by wget\e[0m"
-    else
-        echo -e "\e[1;33mNeither curl nor wget is available for downloading\e[0m"
-        exit 1
-    fi
+generate_random_name() {
+    local chars=abcdefghijklmnopqrstuvwxyz1234567890
+    local name=""
+    for i in {1..6}; do
+        name="$name${chars:RANDOM%${#chars}:1}"
+    done
+    echo "$name"
 }
 
 for entry in "${FILE_INFO[@]}"; do
     URL=$(echo "$entry" | cut -d ' ' -f 1)
-    TYPE=$(echo "$entry" | cut -d ' ' -f 2)  # 获取文件类型，例如 "web" 或 "npm"
-    FIXED_NAME=$(generate_fixed_name "$TYPE")  # 根据类型生成固定文件名
-    NEW_FILENAME="$DOWNLOAD_DIR/$FIXED_NAME"
-
-    download_file "$URL" "$NEW_FILENAME"
-
+    RANDOM_NAME=$(generate_random_name)
+    NEW_FILENAME="$DOWNLOAD_DIR/$RANDOM_NAME"
+    
+    $COMMAND "$NEW_FILENAME" "$URL"
+    echo -e "\e[1;32mDownloading $NEW_FILENAME\e[0m"
     chmod +x "$NEW_FILENAME"
-    FILE_MAP[$TYPE]="$NEW_FILENAME"  # 将类型映射到固定文件名
+    FILE_MAP[$(echo "$entry" | cut -d ' ' -f 2)]="$NEW_FILENAME"
 done
 wait
 
 # Generate cert
-openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout "$WORKDIR/server.key" -out "$WORKDIR/server.crt" -subj "/CN=bing.com" -days 36500
+openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout "$WORKDIR/server.key" -out "$WORKDIR/server.crt" -subj "/CN=${CURRENT_DOMAIN}" -days 36500
 
 get_ip() {
   IP_LIST=($(devil vhost list | awk '/^[0-9]+/ {print $1}'))
@@ -141,7 +139,7 @@ get_ip() {
   else
       FIRST_IP=${IP_LIST[0]}
       RESPONSE=$(curl -s --max-time 2 "${API_URL}/${FIRST_IP}")
-
+      
       if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
           IP=$FIRST_IP
       else
@@ -181,92 +179,89 @@ EOF
 
 install_keepalive () {
     echo -e "\n\e[1;35m正在安装保活服务中,请稍等......\e[0m"
-    keep_path="$HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs"
+    devil www del keep.${USERNAME}.${CURRENT_DOMAIN} > /dev/null 2>&1
+    devil www add keep.${USERNAME}.${CURRENT_DOMAIN} nodejs /usr/local/bin/node18 > /dev/null 2>&1
+    keep_path="$HOME/domains/keep.${USERNAME}.${CURRENT_DOMAIN}/public_nodejs"
     [ -d "$keep_path" ] || mkdir -p "$keep_path"
-    app_file_url="https://raw.githubusercontent.com/lileeleo/daily/refs/heads/main/test/app.js"
-
-    if command -v curl &> /dev/null; then
-        curl -s -o "${keep_path}/app.js" "$app_file_url"
-    elif command -v wget &> /dev/null; then
-        wget -q -O "${keep_path}/app.js" "$app_file_url"
-    else
-        echo -e "\n\e[1;32m警告: 文件下载失败,请手动从https://raw.githubusercontent.com/lileeleo/daily/refs/heads/main/test/app.js下载文件,并将文件上传到${keep_path}目录下\e[0m"
-    fi
+    app_file_url="https://hy2.ssss.nyc.mn/hy2.js"
+    $COMMAND $COMMAND "${keep_path}/app.js" "$app_file_url" 
 
     cat > ${keep_path}/.env <<EOF
 UUID=${UUID}
 SUB_TOKEN=${SUB_TOKEN}
+UPLOAD_URL=${UPLOAD_URL}
 TELEGRAM_CHAT_ID=${CHAT_ID}
 TELEGRAM_BOT_TOKEN=${BOT_TOKEN}
 NEZHA_SERVER=${NEZHA_SERVER}
 NEZHA_PORT=${NEZHA_PORT}
 NEZHA_KEY=${NEZHA_KEY}
-USERNAME=$(whoami)   #大小写
-USENAME=${USERNAME}  #小写
-FILE_PATH=${FILE_PATH}
 EOF
-    devil www add ${USERNAME}.serv00.net php > /dev/null 2>&1
-    devil www add keep.${USERNAME}.serv00.net nodejs /usr/local/bin/node18 > /dev/null 2>&1
-    devil ssl www add $HOST_IP le le keep.${USERNAME}.serv00.net > /dev/null 2>&1
+    devil www add ${USERNAME}.${CURRENT_DOMAIN} php > /dev/null 2>&1
+    index_url="https://github.com/eooce/Sing-box/releases/download/00/index.html"
+    [ -f "${FILE_PATH}/index.html" ] || $COMMAND "${FILE_PATH}/index.html" "$index_url"
     ln -fs /usr/local/bin/node18 ~/bin/node > /dev/null 2>&1
     ln -fs /usr/local/bin/npm18 ~/bin/npm > /dev/null 2>&1
     mkdir -p ~/.npm-global
     npm config set prefix '~/.npm-global'
     echo 'export PATH=~/.npm-global/bin:~/bin:$PATH' >> $HOME/.bash_profile && source $HOME/.bash_profile
     rm -rf $HOME/.npmrc > /dev/null 2>&1
-    cd ${keep_path} && npm install express dotenv axios --silent > /dev/null 2>&1
-    rm $HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs/public/index.html > /dev/null 2>&1
-    devil www options keep.${USERNAME}.serv00.net sslonly on > /dev/null 2>&1
-    if devil www restart keep.${USERNAME}.serv00.net 2>&1 | grep -q "succesfully"; then
+    cd ${keep_path} && npm install dotenv axios --silent > /dev/null 2>&1
+    rm $HOME/domains/keep.${USERNAME}.${CURRENT_DOMAIN}/public_nodejs/public/index.html > /dev/null 2>&1
+    # devil www options keep.${USERNAME}.${CURRENT_DOMAIN} sslonly on > /dev/null 2>&1
+    devil www restart keep.${USERNAME}.${CURRENT_DOMAIN} > /dev/null 2>&1
+    if curl -skL "http://keep.${USERNAME}.${CURRENT_DOMAIN}/${USERNAME}" | grep -q "running"; then
         echo -e "\e[1;32m\n全自动保活服务安装成功\n\e[0m"
-        echo -e "\e[1;32m========================================================\e[0m"
-        echo -e "\e[1;35m\n访问 https://keep.${USERNAME}.serv00.net/status 查看进程状态\n\e[0m"
-        echo -e "\e[1;33m访问 https://keep.${USERNAME}.serv00.net/start 调起保活程序\n\e[0m"
-        echo -e "\e[1;35m访问 https://keep.${USERNAME}.serv00.net/list 全部进程列表\n\e[0m"
-        echo -e "\e[1;35m访问 https://keep.${USERNAME}.serv00.net/stop 结束进程和保活\n\e[0m"
-        echo -e "\e[1;32m========================================================\e[0m"
-        echo -e "\e[1;33m如发现掉线访问https://keep.${USERNAME}.serv00.net/start唤醒,或者用https://console.cron-job.org在线访问网页自动唤醒\n\e[0m"
-        echo -e "\e[1;35m如果需要Telegram通知，请先在Telegram @Botfather 申请 Bot-Token，并带CHAT_ID和BOT_TOKEN环境变量运行\n\n\e[0m"
-
+	    echo -e "\e[1;32m所有服务都运行正常,全自动保活任务添加成功\n\n\e[0m"
+        echo -e "\e[1;35m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/restart 重启进程\n\e[0m"
+        echo -e "\e[1;35m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/list 全部进程列表\n\e[0m"
+        echo -e "\e[1;33m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/${USERNAME} 调起保活程序 备用: http://keep.${USERNAME}.${CURRENT_DOMAIN}/run\n\e[0m"
+        echo -e "\e[1;35m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/status 查看进程状态\n\e[0m"
+        echo -e "\e[1;35m如果需要TG通知,在\e[1;33m https://t.me/laowang_serv00_bot \e[1;35m获取CHAT_ID,并带CHAT_ID环境变量运行\n\n\e[0m"
     else
-        echo -e "\e[1;91m全自动保活服务安装失败,请删除所有文件夹后重试\n\e[0m"
+        echo -e "\e[1;31m\n全自动保活服务安装失败,存在未运行的进程,访问 \e[1;33mhttp://keep.${USERNAME}.${CURRENT_DOMAIN}/status \e[1;31m检查,建议执行以下命令后重装: \n\ndevil www del ${USERNAME}.${CURRENT_DOMAIN}\ndevil www del keep.${USERNAME}.${CURRENT_DOMAIN}\nrm -rf $HOME/domains/*\nshopt -s extglob dotglob\nrm -rf $HOME/!(domains|mail|repo|backups)\n\n\e[0m"
     fi
 }
 
 run() {
-  if [ -e "${FILE_MAP[npm]}" ]; then
-    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
-    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
-      NEZHA_TLS="--tls"
-    else
-      NEZHA_TLS=""
-    fi
-    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
-      export TMPDIR=$(pwd)
-      nohup ./"${FILE_MAP[npm]}" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-      sleep 1
-      pgrep -x "${FILE_MAP[npm]}" > /dev/null && echo -e "\e[1;32m${FILE_MAP[npm]} is running\e[0m" || { echo -e "\e[1;35m${FILE_MAP[npm]} is not running, restarting...\e[0m"; pkill -f "${FILE_MAP[npm]}" && nohup ./"${FILE_MAP[npm]}" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m${FILE_MAP[npm]} restarted\e[0m"; }
-    else
-      echo -e "\e[1;35mNEZHA variable is empty, skipping running\e[0m"
-    fi
+  if [ -e "$(basename ${FILE_MAP[web]})" ]; then
+    nohup ./"$(basename ${FILE_MAP[web]})" server config.yaml >/dev/null 2>&1 &
+    sleep 1
+    pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) is running\e[0m" || { echo -e "\e[1;35m$(basename ${FILE_MAP[web]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" server config.yaml >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) restarted\e[0m"; }
   fi
 
-  if [ -e "${FILE_MAP[web]}" ]; then
-    nohup ./"${FILE_MAP[web]}" server config.yaml >/dev/null 2>&1 &
-    sleep 1
-    pgrep -x "${FILE_MAP[web]}" > /dev/null && echo -e "\e[1;32m${FILE_MAP[web]} is running\e[0m" || { echo -e "\e[1;35m${FILE_MAP[web]} is not running, restarting...\e[0m"; pkill -f "${FILE_MAP[web]}" && nohup ./"${FILE_MAP[web]}" server config.yaml >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m${FILE_MAP[web]} restarted\e[0m"; }
+  if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+      if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
+      tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+        [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]] && NEZHA_TLS="--tls" || NEZHA_TLS=""
+        export TMPDIR=$(pwd)
+        nohup ./"$(basename ${FILE_MAP[npm]})" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+        sleep 2
+        pgrep -x "$(basename ${FILE_MAP[npm]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[npm]}) is running\e[0m" || { echo -e "\e[1;31m$(basename ${FILE_MAP[npm]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[npm]})" && nohup ./"$(basename ${FILE_MAP[npm]})" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m"$(basename ${FILE_MAP[npm]})" restarted\e[0m"; }
+      fi
+  elif [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_KEY" ]; then
+      if [ -e "$(basename ${FILE_MAP[php]})" ]; then
+        nohup ./"$(basename ${FILE_MAP[php]})" -c "${WORKDIR}/config.yaml" >/dev/null 2>&1 &
+        sleep 2
+        pgrep -x "$(basename ${FILE_MAP[php]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[php]}) is running\e[0m" || { echo -e "\e[1;31m$(basename ${FILE_MAP[php]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[php]})" && nohup ./"$(basename ${FILE_MAP[php]})" -s -c "${WORKDIR}/config.yaml" >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m"$(basename ${FILE_MAP[php]})" restarted\e[0m"; }
+      fi
+  else
+      echo -e "\e[1;35mNEZHA variable is empty, skipping running\e[0m"
   fi
-#  rm -rf "${FILE_MAP[web]}" "${FILE_MAP[npm]}"
+
+  for key in "${!FILE_MAP[@]}"; do
+      if [ -e "$(basename ${FILE_MAP[$key]})" ]; then
+          rm -rf "$(basename ${FILE_MAP[$key]})" >/dev/null 2>&1
+      fi
+  done
 }
 run
 
 get_name() { if [ "$HOSTNAME" = "s1.ct8.pl" ]; then SERVER="CT8"; else SERVER=$(echo "$HOSTNAME" | cut -d '.' -f 1); fi; echo "$SERVER"; }
-NAME="$(get_name)-hysteria2"
+NAME="$(get_name)-hysteria2-${USERNAME}"
 
 ISP=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "0")
 
 echo -e "\n\e[1;32mHysteria2安装成功\033[0m\n"
-echo -e "\e[1;32m本机IP：$HOST_IP\033[0m\n"
 echo -e "\e[1;33mV2rayN 或 Nekobox、小火箭等直接导入,跳过证书验证需设置为true\033[0m\n"
 cat > ${FILE_PATH}/${SUB_TOKEN}_hy2.log <<EOF
 hysteria2://$UUID@$HOST_IP:$PORT/?sni=www.bing.com&alpn=h3&insecure=1#$ISP-$NAME
@@ -274,7 +269,7 @@ EOF
 cat ${FILE_PATH}/${SUB_TOKEN}_hy2.log
 echo -e "\n\e[1;35mClash: \033[0m"
 cat << EOF
-- name: $ISP
+- name: $ISP-$NAME
   type: hysteria2
   server: $HOST_IP
   port: $PORT
@@ -285,12 +280,16 @@ cat << EOF
   skip-cert-verify: true
   fast-open: true
 EOF
-echo -e "\n\e[1;35m节点订阅链接: https://${USERNAME}.serv00.net/${SUB_TOKEN}_hy2.log  适用于V2ranN/Nekobox/Karing/小火箭/sterisand/Loon 等\033[0m\n"
-#rm -rf config.yaml fake_useragent_0.2.0.json
+echo ""
+QR_URL="https://00.ssss.nyc.mn/qrencode"
+$COMMAND "${WORKDIR}/qrencode" "$QR_URL" && chmod +x "${WORKDIR}/qrencode"
+"${WORKDIR}/qrencode" -m 2 -t UTF8 "https://${USERNAME}.${CURRENT_DOMAIN}/${SUB_TOKEN}_hy2.log"
+echo -e "\n\e[1;35m节点订阅链接: https://${USERNAME}.${CURRENT_DOMAIN}/${SUB_TOKEN}_hy2.log  适用于V2ranN/Nekobox/Karing/小火箭/sterisand/Loon 等\033[0m\n"
+rm -rf config.yaml fake_useragent_0.2.0.json
 install_keepalive
 echo -e "\e[1;35m老王serv00|CT8单协议hysteria2无交互一键安装脚本\e[0m"
-echo -e "\e[1;35m脚本地址：https://github.com/eooce/sing-box\e[0m"
-echo -e "\e[1;35m反馈论坛：https://bbs.vps8.me\e[0m"
-echo -e "\e[1;35mTG反馈群组：https://t.me/vps888\e[0m"
-echo -e "\e[1;35m转载请著名出处，请勿滥用\e[0m\n"
+echo -e "\e[1;35m脚本地址: https://github.com/eooce/sing-box\e[0m"
+echo -e "\e[1;35m反馈论坛: https://bbs.vps8.me\e[0m"
+echo -e "\e[1;35mTG反馈群组: https://t.me/vps888\e[0m"
+echo -e "\e[1;35m转载请著名出处,请勿滥用\e[0m\n"
 echo -e "\e[1;32mRuning done!\033[0m\n"
